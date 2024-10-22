af0b9d51cd2b87afcd6c37ce6d5a83d2eccd4ddf
Safwan <safwanchoudhury034@gmail.com>
2024-10-22 12:04:33 +0100

    added a label

--- draft3/Form1.Designer.cs
+++ draft3/Form1.Designer.cs
@@ -36,15 +36,16 @@
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(327, 72);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(490, 111);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(55, 36);
+            this.button1.Size = new System.Drawing.Size(82, 55);
             this.button1.TabIndex = 0;
             this.button1.Text = "Search button";
             this.button1.UseVisualStyleBackColor = true;
@@ -53,9 +54,9 @@
             // vScrollBar1
             // 
             this.vScrollBar1.AllowDrop = true;
-            this.vScrollBar1.Location = new System.Drawing.Point(654, 32);
+            this.vScrollBar1.Location = new System.Drawing.Point(981, 49);
             this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(26, 415);
+            this.vScrollBar1.Size = new System.Drawing.Size(26, 638);
             this.vScrollBar1.TabIndex = 1;
             // 
             // directorySearcher1
@@ -72,32 +73,43 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(327, 344);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(490, 529);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(124, 20);
+            this.textBox1.Size = new System.Drawing.Size(184, 26);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "welcome to the page";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(287, 261);
+            this.progressBar1.Location = new System.Drawing.Point(430, 402);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.Size = new System.Drawing.Size(150, 35);
             this.progressBar1.TabIndex = 3;
             this.progressBar1.Value = 75;
             this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(728, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(693, 476);
+            this.ClientSize = new System.Drawing.Size(1040, 732);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.vScrollBar1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -116,6 +128,8 @@
         private System.Windows.Forms.BindingSource bindingSource1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
