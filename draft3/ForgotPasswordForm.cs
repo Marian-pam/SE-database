92bb62b09831ac4e3c870d3c8869aaeac3bcabe1
Safwan <safwanchoudhury034@gmail.com>
2024-11-20 18:14:25 +0000

    attempted to make the forgot password email verification work

--- draft3/ForgotPasswordForm.cs
+++ draft3/ForgotPasswordForm.cs
@@ -1,43 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Mail;
 using System.Windows.Forms;
-using draft3;
 
 namespace draft3
 {
     public partial class ForgotPassword : Form
     {
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
+
         public ForgotPassword()
         {
             InitializeComponent();
         }
-
+        // Triggered when the form is loaded
         private void ForgotPassword_Load(object sender, EventArgs e)
         {
-
+            // Initialization logic, if any
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
+        // Triggered when the label is clicked
         private void label1_Click(object sender, EventArgs e)
         {
-
+            // Add label click functionality here, if needed
         }
 
+        // Triggered when the MaskedTextBox rejects input
         private void EnterEmail_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
         {
-            EnterEmail.Multiline = true;
-            EnterEmail.Height = 200;
-            
+            // Handle input rejection logic, if needed
+        }
+
+        private void SendEmail(string recipientEmail, string messageBody)
+        {
+            try
+            {
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential("SC1969@student.aru.ac.com", "19691969"),
+                    EnableSsl = true
+                };
+
+                MailMessage mail = new MailMessage
+                {
+                    From = new MailAddress("SC1969@student.aru.com"),
+                    Subject = "Password Recovery",
+                    Body = messageBody,
+                    IsBodyHtml = true
+                };
+
+                mail.To.Add(recipientEmail);
+                smtpClient.Send(mail);
+
+                MessageBox.Show("Email sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to send email: {ex.Message}");
+            }
+        }
+
+        private void ValidateEmailAndSend(string enteredEmail)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT COUNT(1) FROM subscriberInfo WHERE emailId = @emailId";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@emailId", enteredEmail);
+                        int count = Convert.ToInt32(command.ExecuteScalar());
+
+                        if (count == 1)
+                        {
+                            string messageBody = "We have received a request to reset your password. Please contact us for further assistance.";
+                            SendEmail(enteredEmail, messageBody);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Email not found in our records.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Database error: {ex.Message}");
+                }
+            }
+        }
+
+        private void ForgotPasswordButton_Click(object sender, EventArgs e)
+        {
+            string enteredEmail = EnterEmail.Text;
+
+            if (!string.IsNullOrWhiteSpace(enteredEmail))
+            {
+                ValidateEmailAndSend(enteredEmail);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid email address.");
+            }
         }
     }
 }
